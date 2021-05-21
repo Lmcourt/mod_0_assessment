134abbe46468cf3e820c6991e59116b6e97ab3bc
Laura Court <laura.mcourt@gmail.com>
2021-05-21 19:55:41 -0400

    Add laptop mmodel hash

---
+++ models/model_a.rb
@@ -0,0 +1,10 @@
+mac_laptop = {
+  color: "silver",
+  front_logo: "apple",
+  listed_price: 1200,
+  discount_price: 1199.99,
+  is_new: true,
+  is_expensive: true,
+  model_number: ["A1466", "A1369"],
+  keyboard_layouts: ["AZERTY", "QWERTY", "QWERTZ"]
+}
