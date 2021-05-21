9d0885fd8e5eade3e4e2544e3b9f1a68605aa2f2
Laura Court <laura.mcourt@gmail.com>
2021-05-21 19:49:24 -0400

    Complete program specific challenge

---
+++ variables.rb
@@ -0,0 +1,9 @@
+anmial = "Bunny"
+is_cuddly = false
+anmial = "Panda"
+num_8 = 56
+num_3 = 2
+quotion = num_3 / num_8
+favorite_films = ["In Bruges", "Spirited Away", "Howl's Moving Castle"]
+favorite_films.shift
+puts "There are #{anmial.length} letters in the word Panda."
