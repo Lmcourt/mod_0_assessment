aa2df40656650a9f6245c011240cee0c44dc6763
Laura Court <laura.mcourt@gmail.com>
2021-05-21 20:06:21 -0400

    Add house model hash

---
+++ models/model_b.rb
@@ -0,0 +1,10 @@
+house_1 = {
+  neighborhood: "Bay Vista Estates",
+  street_name: "Bay Shore Cove",
+  house_number: 8605,
+  num_of_rooms: 5,
+  on_culdesac: true,
+  is_lakefront: true,
+  surrounding_neighborhoods: ["Bay Lakes", "Bristol Park", "Phillip's Landing"],
+  zoned_schools: ["Bay Meadows", "Southwest Middle", "Dr. Phillip's High"]
+}
